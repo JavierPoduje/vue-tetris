49de7c349e005d39fa284082f1ce1869d4a87dc6
javierpoduje <javier.poduje@gmail.com>
2023-07-21 20:16:07 -0400

    feat: improve styles

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,10 +1,20 @@
 <template>
   <header class="header">
-    <h1>Tetris</h1>
-    <p>Level: {{ store.level }}</p>
-    <p>Points: {{ store.points }}</p>
-    <p>Lines: {{ store.linesFilled }}</p>
-    <p v-show="store.state === StateEnum.Gameover">Game Over!</p>
+    <section class="header__title">
+      <h1>Tetris</h1>
+      <p v-show="store.state === StateEnum.Gameover">Game Over!</p>
+    </section>
+    <section class="header__stats">
+      <p>
+        Level: <span class="stat">{{ store.level }}</span>
+      </p>
+      <p>
+        Points: <span class="stat">{{ store.points }}</span>
+      </p>
+      <p>
+        Lines: <span class="stat">{{ store.linesFilled }}</span>
+      </p>
+    </section>
   </header>
   <main class="main">
     <game-grid class="main__grid" />
@@ -16,27 +26,52 @@
   import { useGameStore } from '@/stores/gameStore.ts'
   import GameGrid from '@/controllers/game-grid.vue'
   import ShowcaseGrid from '@/controllers/showcase-grid.vue'
-  import GameButton from '@/components/game-button.vue'
   import { StateEnum } from '../models'
 
   const store = useGameStore()
 </script>
 
 <style scoped lang="scss">
+  @import '../assets/variables.scss';
+
   .header {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-between;
+    align-items: flex-start;
     margin-bottom: 3rem;
+
+    &__title {
+      display: flex;
+      flex-direction: column;
+      font-weight: bold;
+      margin-left: 1rem;
+
+      h1 {
+        font-size: 36px;
+        text-shadow: $textShadow;
+      }
+    }
+    &__stats {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      p {
+        font-weight: bold;
+        align-self: flex-end;
+      }
+
+      .stat {
+        text-shadow: $textShadow;
+      }
+    }
   }
 
   .main {
     align-self: center;
-    display: grid;
-    grid-template-columns: 5fr 1fr;
-    grid-template-areas: 'grid showcase';
-    width: 35%;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
 
     &__grid {
       grid-area: grid;
